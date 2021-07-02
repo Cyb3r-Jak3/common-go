e098a611f5c35eb82051f47a19ec776eaa78d751
Jacob White <jake@jwhite.network>
2021-07-01 23:48:35 -0400

    More benchmarks

--- go/bench_test.go
+++ go/bench_test.go
@@ -2,6 +2,7 @@ package common_test
 
 import (
 	"net/http"
+	"os"
 	"testing"
 
 	common "github.com/Cyb3r-Jak3/common/go"
@@ -58,5 +59,88 @@ func BenchmarkJSONMarshall(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		executeRequest(r, JSONMarshalTest)
+	}
+}
+
+func BenchmarkGenerate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = common.GenerateRandInt(5)
+	}
+}
+
+func BenchmarkJSONParse(b *testing.B) {
+	testStruct := new(testStruct)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = common.ParseYamlOrJSON("../testData/parsetest.json", testStruct)
+	}
+
+}
+
+func BenchmarkYAMLParse(b *testing.B) {
+	testStruct := new(testStruct)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = common.ParseYamlOrJSON("../testData/parsetest.yml", testStruct)
+	}
+
+}
+
+func BenchmarkStringSearch2(b *testing.B) {
+	array := []string{"hello", "world"}
+	for i := 0; i < b.N; i++ {
+		common.StringSearch("hello", array)
+	}
+
+}
+func BenchmarkStringSearch10(b *testing.B) {
+	array := []string{"hello", "world", "hi", "mom", "and", "here", "are", "ten", "random", "words"}
+	for i := 0; i < b.N; i++ {
+		common.StringSearch("hello", array)
+	}
+
+}
+func BenchmarkFloatSearch2(b *testing.B) {
+	array := []float64{1.1, 1.2}
+	for i := 0; i < b.N; i++ {
+		common.FloatSearch(1.9, array)
+	}
+}
+
+func BenchmarkFloatSearch10(b *testing.B) {
+	array := []float64{1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0}
+	for i := 0; i < b.N; i++ {
+		common.FloatSearch(1.9, array)
+	}
+}
+
+func BenchmarkIntSearch2(b *testing.B) {
+	array := []int{1, 2}
+	for i := 0; i < b.N; i++ {
+		common.IntSearch(1, array)
+	}
+}
+
+func BenchmarkIntSearch10(b *testing.B) {
+	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for i := 0; i < b.N; i++ {
+		common.IntSearch(1, array)
+	}
+}
+
+func BenchmarkGetEnv(b *testing.B) {
+	os.Setenv("test", "value")
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = common.GetEnv("test", "")
+	}
+}
+
+func BenchmarkGetEnvMissing(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = common.GetEnv("missing", "value")
 	}
 }
