76edf340b0933cf3cebe6818786c5e3db2b95f31
Cyb3r Jak3 <jake@jwhite.network>
2021-09-13 16:31:37 -0400

    Adds common colors ints (#16)
    * Adds common colors ints
    
    * Fix format

---
+++ go/colors.go
@@ -0,0 +1,33 @@
+package common
+
+// Integer codes for common colors
+const (
+	Aqua              = 1752220
+	Black             = 2303786
+	Blue              = 3447003
+	DarkAqua          = 1146986
+	DarkBlue          = 2123412
+	DarkButNotBlack   = 2895667
+	DarkGold          = 12745742
+	DarkGreen         = 2067276
+	DarkGrey          = 9936031
+	DarkNavy          = 2899536
+	DarkOrange        = 11027200
+	DarkPurple        = 7419530
+	DarkRed           = 10038562
+	DarkVividPink     = 11342935
+	DarkerGrey        = 8359053
+	Fuschia           = 15418782
+	Gold              = 15844367
+	Green             = 5763719
+	Grey              = 9807270
+	LightGrey         = 12370112
+	LuminousVividPink = 15277667
+	Navy              = 3426654
+	NotQuiteBlack     = 2303786
+	Orange            = 15105570
+	Purple            = 10181046
+	Red               = 15548997
+	White             = 16777215
+	Yellow            = 16705372
+)
