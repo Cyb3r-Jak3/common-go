a4a3be12b1a44b3dcac10e95cb170c7498453f96
Jacob White <jake@jwhite.network>
2021-09-13 15:44:00 -0400

    Adds common colors ints

---
+++ go/colors.go
@@ -0,0 +1,33 @@
+package common
+
+const (
+	AQUA                = 1752220
+	BLACK               = 2303786
+	BLUE                = 3447003
+	DARK_AQUA           = 1146986
+	DARK_BLUE           = 2123412
+	DARK_BUT_NOT_BLACK  = 2895667
+	DARK_GOLD           = 12745742
+	DARK_GREEN          = 2067276
+	DARK_GREY           = 9936031
+	DARK_NAVY           = 2899536
+	DARK_ORANGE         = 11027200
+	DARK_PURPLE         = 7419530
+	DARK_RED            = 10038562
+	DARK_VIVID_PINK     = 11342935
+	DARKER_GREY         = 8359053
+	FUSCHIA             = 15418782
+	GOLD                = 15844367
+	GREEN               = 5763719
+	GREY                = 9807270
+	GREYPLE             = 10070709
+	LIGHT_GREY          = 12370112
+	LUMINOUS_VIVID_PINK = 15277667
+	NAVY                = 3426654
+	NOT_QUITE_BLACK     = 2303786
+	ORANGE              = 15105570
+	PURPLE              = 10181046
+	RED                 = 15548997
+	WHITE               = 16777215
+	YELLOW              = 16705372
+)
